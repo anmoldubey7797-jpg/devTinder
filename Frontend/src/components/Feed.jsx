62c34d610a5fd30cfee634ad783d061edd39b52c
Anmol Dubey <anmoldubey7797@gmail.com>
2026-03-11 18:39:44 +0530

    Remove commented-out code and clean up Feed component structure

--- Frontend/src/components/Feed.jsx
+++ Frontend/src/components/Feed.jsx
@@ -1,37 +1,3 @@
-// import axios from 'axios'
-// import React from 'react'
-// import { BASE_URL } from '../utils/constants'
-// import { useDispatch, useSelector } from 'react-redux'
-// import { addFeed } from '../utils/feedSlice'
-// import { useEffect } from 'react'
-// import UserCard from './userCard'
-
-// const Feed = () => {
-//   const feed=useSelector((store)=>store.feed);
-//   const dispatch=useDispatch();
-  
-//   const getFeed=async()=>{
-   
-//   try{
-//   const res=await axios.get(BASE_URL +"/feed",{withCredentials:true})
-//   console.log("FEED API RESPONSE:", res.data);
-//   dispatch(addFeed(res.data.data));
-// }catch(error){
-//   console.log(error)
-// }
-// }
-
-// useEffect(()=>{
-//   getFeed();
-// },[])
-//   return feed && (
-//    <>
-//    <UserCard user={feed[0]}/>
-//    </>
-//   )
-// }
-
-// export default Feed
 
 import axios from "axios";
 import React, { useEffect } from "react";
