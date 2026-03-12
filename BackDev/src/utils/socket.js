58fd008753342a0399f99aee44b6771ffc998a7d
Anmol Dubey <anmoldubey7797@gmail.com>
2026-03-12 12:41:16 +0530

    Refactor socket initialization and event handling in socket.js for improved readability and structure

--- BackDev/src/utils/socket.js
+++ BackDev/src/utils/socket.js
@@ -1,18 +1,30 @@
-const server=http.createServer(app);
-import socket from 'socket.io';
+import socket from "socket.io";
 
-
-const initializeSocket=(server)=>{
-    const io=socket(server,{
-    cors:{
-        origin:"http://localhost:5173",
-        credentials:true
+const initializeSocket = (server) => {
+  const io = socket(server, {
+    cors: {
+      origin: "http://localhost:5173",
+      credentials: true,
     },
-})  
+  });
 
-io.on("connection",(socket)=>{
-    console.log("New client connected: " + socket.id);
-})
-}
+  io.on("connection", () => {
+    console.log("New client connected: ");
+
+    socket.on("joinChat", () => {
+      console.log("User joined chat");
+    });
+
+    socket.on("sendMessage", () => {
+      console.log("Received message: ");
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Client disconnected: " );
+    });
+  });
+};
 
 export default initializeSocket;
+
+
